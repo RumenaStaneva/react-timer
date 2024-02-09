9e1610a9e31162ef592f79ed49b02e8e0d85d376
RumenaStaneva <rumenastaneva2106@gmail.com>
2024-02-09 10:10:31 +0200

    reorganizing components and added timer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
-import Input from "./components/Input";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import { useRef } from "react";
-import Form, { type FormHandle } from "./components/Form";
+import AddTimer from "./components/AddTimer";
+import Timers from "./components/Timers";
 
 function App() {
-  // const inputRef = useRef<HTMLInputElement>(null);
-  const formRef = useRef<FormHandle>(null);
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    formRef.current?.clear();
-  }
   return (
     <main>
-      <Form onSave={handleSave} ref={formRef}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-
-      {/* <Input type="text" label="Test" id="test" ref={inputRef} /> */}
-
-      {/* <Container as={Button}>Click</Container> */}
-      {/* <Input type="text" label="name" id="name" />
-      <Input type="number" label="your age" id="age" />
-
-      <p>
-        <Button>A button</Button>
-      </p>
-
-      <p>
-        <Button href="https://google.com" target="_blank">
-          A Link
-        </Button>
-      </p> */}
+      <AddTimer />
+      <Timers />
     </main>
   );
 }
